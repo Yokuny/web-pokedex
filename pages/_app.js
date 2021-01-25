e755f774b417512be46589403d9075b376f961ef
yokuny <felipe.vni@hotmail.com>
2021-01-25 15:55:53 -0300

    all the pokemon in the screen, i have put colors and hover action

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="grid grid-cols-8">
+      <div className="col-start-2 col-span-6">
+        <Component {...pageProps} />
+      </div>
+    </div>
+    
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,38 @@
-import Head from 'next/head'
-
-export default function Home() {
+import Image from "next/image"
+import Link from "next/link"
+export default function IndexPage({pokemon}) {
+  console.log(pokemon);
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
+      <div className="grid lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-3">
+          {pokemon.map((thePokemon, index) => 
+            <div className="p-2 bg-gray-400 hover:bg-gray-500 rounded">
+              <Image
+                src={thePokemon.image}
+                alt='pokemon image'
+                height={200}
+                width={200}
+              />
+              <div className="text-center">
+                <code className="text-xs">{index+1} </code> <code className="font-bold">{thePokemon.name}</code>
+              </div>
+            </div>
+          )}
+      </div>
   )
+}
+export async function getStaticProps() {
+  try {
+      const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=150');
+      const { results } = await res.json();
+      const pokemon = results.map((pokeman, index) => {
+          const paddedId = ('00' + (index + 1)).slice(-3);
+          const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${paddedId}.png`;
+          return { ...pokeman, image };
+      });
+      return {
+          props: { pokemon },
+      };
+  } catch (err) {
+      console.error(err);
+  }
 }
